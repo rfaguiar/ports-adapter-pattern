ab30bc403843a62dac864384158d44baad0a8e5c
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-03-28 20:27:58 -0300

    Arquitetura Hexagonal na Prática - Definindo interfaces de serviços e persistência

--- application/product.go
+++ application/product.go
@@ -3,6 +3,7 @@ package application
 import (
 	"errors"
 	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 )
 
 func init() {
@@ -19,6 +20,26 @@ type ProductInterface interface {
 	GetPrice() float64
 }
 
+type ProductService interface {
+	Get(id string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductReader interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(name string, price float64) (ProductInterface, error)
+}
+
+type ProductPersistenceInterface interface {
+	ProductReader
+	ProductWriter
+}
+
 const (
 	DISABLED = "disable"
 	ENABLE   = "enable"
@@ -29,6 +50,14 @@ type Product struct {
 	Name   string  `valid:"required"`
 	Price  float64 `valid:"float,optional"`
 	Status string  `valid:"required"`
+}
+
+func NewProduct() *Product {
+	product := Product{
+		ID:     uuid.NewV4().String(),
+		Status: DISABLED,
+	}
+	return &product
 }
 
 func (p *Product) IsValid() (bool, error) {
