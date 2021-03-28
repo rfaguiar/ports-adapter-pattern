5f9c5861375480879cb3cd9222f71140fa1b3e27
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-03-28 20:07:48 -0300

    Arquitetura Hexagonal na Prática - Implementando e testando demais métodos

--- application/product_test.go
+++ application/product_test.go
@@ -2,6 +2,7 @@ package application_test
 
 import (
 	"github.com/rfaguiar/ports-adapter-pattern/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -19,4 +20,46 @@ func TestProduct_Enable(t *testing.T) {
 
 	err = product.Enable()
 	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLE
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	var _, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLE
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+
+	product.Price = 10
+	_, err = product.IsValid()
+	require.Nil(t, err)
 }
